8cbca8007ca6893af214faa97230e276fccddbe6
masterbg94 <>
2024-08-08 17:49:54 +0200

    slike leonardo 7 update

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  leonardo7Heading:string = 'gallery2/slika3.jpg';
+  leonardo7Heading:string = 'gallery2/slika3.png';
 }
